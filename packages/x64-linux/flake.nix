72ae178faf42fae80376113f65634df8759008e9
Jorge Fernando Álava <git@jfa.dev>
2025-12-13 18:25:43 +0100

    Create flake.nix

---
+++ packages/x64-linux/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Home Manager configuration for jfalava";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations = {
+        jfalava = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          # Specify your home configuration modules here.
+          # For example, the path to your home.nix.
+          modules = [
+            ./home.nix
+          ];
+
+          # Optionally use extraSpecialArgs to pass through arguments to home.nix
+          extraSpecialArgs = {
+            # You can add extra arguments here if needed
+          };
+        };
+      };
+    };
+}
