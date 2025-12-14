4ce0913f4a06ee45333885c9eedb043f96e84f20
Jorge Fernando Álava <git@jfa.dev>
2025-12-14 13:00:25 +0100

    fix: bad branch for nix packages

--- packages/x64-linux/flake.nix
+++ packages/x64-linux/flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration for jfalava";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/unstable";
+    nixpkgs.url = "github:nixos/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
