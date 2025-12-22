b728559f2fca07f1c72e6d96d3c1aefffb4ef083
Jorge Fernando Álava <jorgefernando.alava@seidor.com>
2025-12-22 11:10:36 +0100

    feat: user as trusted user

--- wsl-install-script.sh
+++ wsl-install-script.sh
@@ -154,10 +154,11 @@ source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh || source ~/.ni
 # No need to manually append to .zshrc here
 
 sudo mkdir -p /etc/nix
-sudo tee -a /etc/nix/nix.conf > /dev/null << 'EOF'
+sudo tee -a /etc/nix/nix.conf > /dev/null << EOF
 
 substituters = https://cache.nixos.org/ https://nix-community.cachix.org
 trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+trusted-users = root $USER
 auto-optimise-store = true
 max-jobs = auto
 experimental-features = nix-command flakes
