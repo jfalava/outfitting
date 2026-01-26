e1a94bfc76db6f7a852d56275cc6b2f70bb3532b
Jorge Fernando Álava <git@jfa.dev>
2026-01-26 13:09:55 +0100

    fix: pin nix home manager and pkgs version to 25.11

--- packages/aarch64-darwin/flake.nix
+++ packages/aarch64-darwin/flake.nix
@@ -2,11 +2,11 @@
   description = "macOS nix-darwin + Home Manager configuration using flakes";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
