3d6232f26be33d1d9e6109e6ba1f6315a8963a05
Jorge Fernando Álava <git@jfa.dev>
2026-07-15 05:33:42 +0200

    chore: script cleanup

--- windows-post-install-script.ps1
+++ windows-post-install-script.ps1
@@ -1,24 +1,26 @@
 # Post install script for Windows
-# This requires a shell reload.
+
+## This requires a shell reload.
 
 $ErrorActionPreference = "Stop"
-
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
-# Scoop
+###################################### Scoop
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 . $PROFILE
+############################################
 
+################### Universal Font Installer
+
+############### Admin to install to all users
 function Test-Administrator {
     $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
     $principal = [Security.Principal.WindowsPrincipal]::new($identity)
     return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
-
 function Stop-FontInstall([string] $Message) {
     throw "Private font installation failed: $Message"
 }
-
 function Test-SafeFontArchive([string] $ArchivePath) {
     $entries = & tar -tzf $ArchivePath
     if ($LASTEXITCODE -ne 0) { Stop-FontInstall "unable to list archive contents" }
@@ -46,11 +48,12 @@ function Test-SafeFontArchive([string] $ArchivePath) {
         }
     }
 }
-
 if (-not (Test-Administrator)) {
     throw "Run PowerShell as Administrator to install fonts for all users."
 }
+############################################
 
+############################# Font Installer
 $tempDirectory = Join-Path $env:TEMP ("outfitting-fonts-" + [guid]::NewGuid().ToString("N"))
 $archivePath = Join-Path $tempDirectory "fonts.tar.gz"
 $checksumPath = Join-Path $tempDirectory "fonts.tar.gz.sha256"
@@ -60,7 +63,6 @@ $checksumUrl = "https://win.jfa.dev/fonts/checksum"
 $installed = 0
 $skipped = 0
 $failed = 0
-
 try {
     if (-not (Get-Command cloudflared -ErrorAction SilentlyContinue)) {
         winget install --id Cloudflare.cloudflared -e --accept-source-agreements --accept-package-agreements
@@ -133,3 +135,4 @@ try {
 } finally {
     Remove-Item -LiteralPath $tempDirectory -Recurse -Force -ErrorAction SilentlyContinue
 }
+############################################
