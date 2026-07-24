33b5691143ea776946b1ac91d7124ca3fa234269
Jorge Fernando Álava <git@jfa.dev>
2026-07-24 17:20:12 +0200

    chore: folder renaming

--- documentation-old/.source/dynamic.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-/// <reference types="vite/client" />
-import { dynamic } from 'fumadocs-mdx/runtime/dynamic';
-import * as Config from '../source.config';
-
-const create = await dynamic<typeof Config, import("fumadocs-mdx/runtime/types").InternalTypeConfig & {
-  DocData: {
-  }
-}>(Config, {"configPath":"/Users/jfalava/.config/outfitting/repo/documentation/source.config.ts","environment":"vite","outDir":"/Users/jfalava/.config/outfitting/repo/documentation/.source"}, {"doc":{"passthroughs":["extractedReferences"]}});
