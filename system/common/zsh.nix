fd11bcec14c6ea71f6e610d28a9e9bd522f96f8f
Jorge Fernando Álava <git@jfa.dev>
2026-08-11 10:43:19 +0200

    Update zsh.nix

--- system/common/zsh.nix
+++ system/common/zsh.nix
@@ -101,7 +101,6 @@ in
 
       wrangler = "bun wrangler";
       alchemy = "bun alchemy";
-      skills = "bun skills";
       ff = "fastfetch";
       cat = "bat --style=auto";
       diff = "diff --color=auto";
